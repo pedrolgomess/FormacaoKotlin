69205a3cb6427f345a0ec290a4b6aed12f34f02a
Pedro Lucas <pedro.lucas@guaraves.local>
2024-05-10 08:58:48 -0300

    FEAT: Minha primeira requisição

--- AluGames/src/main/kotlin/Main.kt
+++ AluGames/src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
 package org.example
 
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandler
+import java.net.http.HttpResponse.BodyHandlers
+
 fun main() {
-    println("Hello World!")
+    val httpClient: HttpClient = HttpClient.newHttpClient()
+
+    val request = HttpRequest.newBuilder().uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146")).build()
+
+    val response = httpClient.send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+
+    println(json)
+
 }
